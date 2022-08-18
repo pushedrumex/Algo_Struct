9f3117ce08a822ee02df9d325882c5d46a9ab89a
pushedrumex <pushedrumex@naver.com>
2022-08-18 21:20:50 +0900

    search

--- search/PhysExamSearch.java
+++ search/PhysExamSearch.java
@@ -30,6 +30,7 @@ class PhysExamSearch {
         private static class VisionOrderComparator implements Comparator<PhyscData> {
             public int compare(PhyscData d1, PhyscData d2) {
                 return (d1.vision > d2.vision) ? -1 : (d1.vision < d2.vision) ? 1 : 0;
+                // 오름차순이라면 +,- 위치 변경
             }
         }
     }
